f2d2cafd46160e2ad0f104e74122595292fd533c
natalliaSkiba <nt1711182@gmail.com>
2022-10-07 12:30:31 +0200

    07_10_22

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,9 @@ import org.example.game.hero.Elf4;
 
 public class Main {
     public static void main(String[] args) {
-        Elf4 Elf1 = new Elf4(100);
-        Elf4 Elf2 = new Elf4("Tim");
-//        Elf4 Elf4 = new Elf4(100);
+        Elf4 Elf4 = new Elf4(100,"Tim");
+        System.out.println(Elf4.getId());
+        System.out.println(Elf4.getNickname());
+
    }
 }
