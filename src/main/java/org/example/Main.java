e0b1b6f20fd06d2ba8f8f794feeece9ebfcaa4c8
natalliaSkiba <nt1711182@gmail.com>
2022-10-07 11:11:43 +0200

    dsjhf

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.game.hero.Elf4;
+
+public class Main {
+    public static void main(String[] args) {
+        Elf4 Elf1 = new Elf4(100);
+        Elf4 Elf2 = new Elf4("Tim");
+//        Elf4 Elf4 = new Elf4(100);
+   }
+}
---
+++ src/main/java/org/example/game/hero/Elf4.java
@@ -0,0 +1,22 @@
+package org.example.game.hero;
+
+public class Elf4 {
+    private int id;
+    private String nickname;
+
+
+    public Elf4(int id) {
+        this.id = id;
+
+    }
+    public Elf4(String nickname) {
+        this.nickname = nickname;
+
+    }
+
+    public Elf4(int id, String nickname) {
+        this.id = id;
+        this.nickname = nickname;
+    }
+}
+
